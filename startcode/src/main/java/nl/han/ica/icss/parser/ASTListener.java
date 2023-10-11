ebd7fa92a8c55ba60348fa947fc1d07e34227654
Vasil Verdouw <vasilverdouw@gmail.com>
2023-10-11 10:38:28 +0200

    Updated gitignore

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -2,7 +2,6 @@ package nl.han.ica.icss.parser;
 
 import java.util.Stack;
 
-
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
@@ -17,26 +16,26 @@ import nl.han.ica.icss.ast.selectors.TagSelector;
  * This class extracts the ICSS Abstract Syntax Tree from the Antlr Parse tree.
  */
 public class ASTListener extends ICSSBaseListener {
-	
-	//Accumulator attributes:
+
+	// Accumulator attributes:
 	private AST ast;
 
-	//Use this to keep track of the parent nodes when recursively traversing the ast
+	// Use this to keep track of the parent nodes when recursively traversing the
+	// ast
 	private IHANStack<ASTNode> currentContainer;
 
 	public ASTListener() {
 		ast = new AST();
-		//currentContainer = new HANStack<>();
+		// currentContainer = new HANStack<>();
 	}
-    public AST getAST() {
-        return ast;
-    }
+
+	public AST getAST() {
+		return ast;
+	}
 
 	@Override
 	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
-
-
-
+		
 	}
-    
+
 }
